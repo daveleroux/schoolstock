d024c3835fe79987ab6abd0d7d543aa447932b1f
r2d2 <r2d2@tau-devel-container-tc.lautus.net>
2026-03-01 13:59:22 +0200

    initial commit

---
+++ src/main/java/org/schoolstock/schoolstock/SchoolstockApplication.java
@@ -0,0 +1,13 @@
+package org.schoolstock.schoolstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolstockApplication.class, args);
+	}
+
+}
